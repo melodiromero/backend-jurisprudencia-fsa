a9c6ea2b06d10f3312c68021e7710a71fada07da
Melodi <zucarubi@gmail.com>
2023-07-07 13:05:57 -0300

    cambios

--- controllers/fallo.controller.js
+++ controllers/fallo.controller.js
@@ -15,9 +15,9 @@ exports.getFalloById = async (req, res, next) => {
     }
     
     const [leerFallo] = await Fallos.getById(id, null, null);
-    
-    
-    if (leerFallo[0].length === 0) {
+        
+    if (leerFallo[0].length === 0) 
+    {
       throw new Error ("Error en parámetro de consulta.");
     };
 
@@ -25,9 +25,8 @@ exports.getFalloById = async (req, res, next) => {
 
     let array_sumarios, sumarios = [];
 
-    console.log('sum', leerSumarios[0]);
-
-    if (leerSumarios[0].length > 0) {
+    (leerSumarios[0].length > 0)
+    {
       // Guardamos los datos del sumario en un array de sumarios.-
       array_sumarios = leerSumarios[0]
       for (let clave in array_sumarios){
@@ -149,15 +148,77 @@ exports.getFallos = async (req, res, next) => {
       throw new Error('Faltan datos. Para buscar por voces debe ingresar términos con más de 3 caracteres.');
     }
 
-    const [leerFallos] = await Fallos.get(id_fallo, numeroFallo, tribunal, tipoFallo, fechaFallo, caratula, descriptores, palabraLibre);
+    const [leerFallos] = await Fallos.get(jurisdiccion, provincia, publicacion_desde, publicacion_hasta, fecha_umod, texto, descriptores, tribunal, limit, offset);
+    
     console.log('mensaje', leerFallos);
 
     if (leerFallos[0].length === 0) {
       throw new Error ("No se hallaron resultados, verifique sus parámetros de busqueda.");
     };
 
-    res.status(200).json(leerFallos[0]);
+    let fallos, sumarios = [];
+    let array_fallos =  eerFallos[0];
+    (array_fallos.length > 0)
+    {
    
+      for (let clave in array_fallos){
+
+        fallos.push({
+                      "metadata": 
+                      {
+                        "uuid":             array_fallos[clave].id_fallo,
+                        "document-type" :   "jurisprudencia"
+                      },
+                      
+                      "content":  
+                      {
+                        "id_fallo":          array_fallos[clave].id_fallo,
+                        "tribunal":          array_fallos[clave].tribunal, 
+                        "tipo_tribunal":     null,
+                        "instancia":         null,
+                        "fecha":             array_fallos[clave].fecha, 
+                        'jurisdiccion': 
+                                                {
+                                                  "tipo":       "LOCAL",
+                                                  "pais":       "Argentina",
+                                                  "provincia":  "FORMOSA",
+                                                  "localidad":  array_fallos[0].localidad,
+                                                  "id_pais": 11
+                                                },
+                        "caratula":             {
+                                                  "actor": null,
+                                                  "demandado": null,
+                                                  "sobre": array_fallos[0].caratula, 
+                                                 },
+                        "sumarios_relacionados": sumarios,
+                        "descriptores": [],
+                        "fecha_umod":           array_fallos[0].fecha_umod
+                 
+                        
+                      }
+                   });
+    
+    res.status(200).json(
+      {
+        "document": 
+        { 
+           "SearchResultList": 
+           {
+              "results"     : "",
+              "query"       : "",
+              "offset"      : "",
+              "pageSize"    : ""        
+           },
+           "DocumentResultList": 
+           {
+              "fallos"      : fallos
+          }
+        }
+      });
+   
+
+
+
   }catch(e){
     console.error(e.message);
     // BAD_REQUEST (400)
@@ -167,62 +228,3 @@ exports.getFallos = async (req, res, next) => {
   }  
 
 };
-
-/*
-// Obtiene los fallos segun los parametros de busqueda.
-exports.getFallos = async (req, res, next) => {  
-  // req.query is mostly used for searching,sorting, filtering, pagination, e.t.c
-  
-  console.log('datos', req.query);
-  
-  let id_fallo      =  req.query.id_fallo;
-  let numeroFallo   =  req.query.numeroFallo;
-  let tribunal      =  req.query.tribunal;
-  let tipoFallo     =  req.query.tipoFallo;
-  let fechaFallo    =  req.query.fechaFallo;
-  let caratula      =  req.query.caratula;
-  let descriptores  =  req.query.descriptores;
-  let palabraLibre  =  req.query.palabraLibre;
-  
-  try{
-    
-    if(!id_fallo && !numeroFallo && !tribunal && !tipoFallo && !fechaFallo && !caratula && !descriptores && !palabraLibre) { // Validación de datos - el usuario debe ingresar al menos un parámetro de búsqueda. }
-      throw new Error('Faltan datos. Por favor, debe ingresar almenos un parámetro de búsqueda . ');
-    } //redirección al catch
-    
-    if (fechaFallo && fechaFallo.length != 10) {
-      throw new Error('Faltan datos. El parámetro de la fecha de fallo no está correcta. La forma correcta es: dd/mm/aaaa.');
-    }
-
-    if (caratula && caratula.length <= 3) {
-      throw new Error('Faltan datos. Para buscar por carátula debe ingresar una frase con más de 3 caracteres.');
-    }
-
-    if (descriptores && descriptores.length <= 3) {
-      throw new Error('Faltan datos. Para buscar por voces debe ingresar términos con más de 3 caracteres.');
-    }
-
-    if (palabraLibre && palabraLibre.length <= 3) {
-      throw new Error('Faltan datos. Para buscar por voces debe ingresar términos con más de 3 caracteres.');
-    }
-
-    const [leerFallos] = await Fallos.get(id_fallo, numeroFallo, tribunal, tipoFallo, fechaFallo, caratula, descriptores, palabraLibre);
-    console.log('mensaje', leerFallos);
-
-    if (leerFallos[0].length === 0) {
-      throw new Error ("No se hallaron resultados, verifique sus parámetros de busqueda.");
-    };
-
-    res.status(200).json(leerFallos[0]);
-   
-  }catch(e){
-    console.error(e.message);
-    // BAD_REQUEST (400)
-
-    res.status(400).send({'mensaje': e.message});
-    next(e);
-  }  
-
-};
-
-*/
